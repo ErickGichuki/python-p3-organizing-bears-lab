8d874bc22d7c2d9a77e2d8003fa4db66bf7fa01a
+ <+>
2024-06-03 10:50:45 +0300

    first commit

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears(id,name,age,sex,color,temperament,alive) VALUES
+(1,"Mr.Chocolate", 23, "M","Brown","calm","yes"),
+(2,"Rowdy", 33, "M","Black","serious","yes"),
+(3,"Tabitha", 40, "F","Brown","calm","yes"),
+(4,"Sergeant Brown", 20, "M","Brown","calm","yes"),
+(5,"Melissa", 23, "M","Brown","serious","yes"),
+(6,"Grinch", 10, "M","Black","calm","yes"),
+(7,"Wendy", 47, "M","Brown","shy","yes"),
+(8,null, 39, "M","Brown","naive","yes");
